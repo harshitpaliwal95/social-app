f0f87be8d2f9f3f4bd680d40564a1d101ec49c68
Harshit Paliwal <harshitpaliwal95@gmail.com>
2022-05-08 23:04:22 +0530

    initial setup

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { CircularProgress } from '@mui/material';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     
+     <CircularProgress />
+    </div>
+  );
+}
+
+export default App;
