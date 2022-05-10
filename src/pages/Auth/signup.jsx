0ced095e601b45386ce5a2854db46c155ceba221
Harshit Paliwal <harshitpaliwal95@gmail.com>
2022-05-11 00:18:08 +0530

    FIX: import UI comps added home page

--- src/pages/Auth/signup.jsx
+++ src/pages/Auth/signup.jsx
@@ -1,16 +1,19 @@
 import * as React from "react";
-import Avatar from "@mui/material/Avatar";
-import Button from "@mui/material/Button";
-import CssBaseline from "@mui/material/CssBaseline";
-import TextField from "@mui/material/TextField";
-import FormControlLabel from "@mui/material/FormControlLabel";
-import Checkbox from "@mui/material/Checkbox";
-import Grid from "@mui/material/Grid";
-import Box from "@mui/material/Box";
-import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
-import Typography from "@mui/material/Typography";
-import Container from "@mui/material/Container";
-import { createTheme, ThemeProvider } from "@mui/material/styles";
+import {
+  Avatar,
+  Button,
+  CssBaseline,
+  TextField,
+  FormControlLabel,
+  Checkbox,
+  Grid,
+  Box,
+  LockOutlinedIcon,
+  Typography,
+  Container,
+  createTheme,
+  ThemeProvider,
+} from "../../getUi";
 import { SignupHandleSubmit } from ".";
 import { Link } from "react-router-dom";
 
@@ -21,7 +24,6 @@ const darkTheme = createTheme({
 });
 
 export const Signup = () => {
- 
   return (
     <ThemeProvider theme={darkTheme}>
       <Container component="main" maxWidth="xs">
